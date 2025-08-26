55de65c357534f4ada46574ffea862b114060fa6
jiyoon <rhjiyoon@yonsei.ac.kr>
2025-08-26 13:28:11 +0900

    feat: CORS middleware 추가

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import models
 from .database import engine
@@ -10,6 +11,23 @@ init_db()
 from .api import students, feedbacks, feedback_details, auth, grades
 
 app = FastAPI()
+
+origins = [
+    "https://jiy0-0nv.github.io",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://127.0.0.1",
+    "http://127.0.0.1:5500",
+    "null"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/api/v1")
 app.include_router(grades.router)
